ab917d546d4e56d5b54d2cd88042fbd6eec5f985
= <daniell.felipe.2017@gmail.com>
2025-03-26 22:15:30 -0300

    add SafeAreaView and header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { styles } from './styles'
 
+import logo from '@/assets/logo.png'
+
 export function Home() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} />
+      </View>
       <Text>Todo App</Text>
-    </View>
+    </SafeAreaView>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,16 @@
 import { StyleSheet } from 'react-native'
 
+import { colors } from '@/styles/colors'
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.gray['700'],
+  },
+  header: {
     justifyContent: 'center',
     alignItems: 'center',
+    width: '100%',
+    minHeight: 170,
   },
 })
