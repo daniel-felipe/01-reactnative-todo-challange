042e922efc7d3ca376e28c8ea98fa62d05f50df2
= <daniell.felipe.2017@gmail.com>
2025-03-26 21:43:40 -0300

    configure colors and font

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
 import { Home } from '@/screens/Home'
 
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  useFonts,
+} from '@expo-google-fonts/inter'
+
 export default function App() {
+  useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  })
+
   return <Home />
 }
---
+++ src/styles/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  gray: {
+    100: '#F2F2F2',
+    200: '#D9D9D9',
+    300: '#808080',
+    400: '#333333',
+    500: '#262626',
+    600: '#1A1A1A',
+    700: '#0D0D0D',
+  },
+
+  purple: {
+    base: '#8284FA',
+    dark: '#5E60CE',
+  },
+
+  blue: {
+    base: '#4EA8DE',
+    dark: '#1E6F9F',
+  },
+
+  danger: '#E25858',
+}
